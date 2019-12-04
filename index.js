7679ee8d703f52cd826156a97668d3dbc7ee0516
Thomas Barrett <tsbarrett89@gmail.com>
2019-12-04 05:44:42 -0700

    added express and started server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // implement your API here
+const express = require("express");
+
+const server = express();
+
+const port = 4000;
+server.listen(port, () => 
+    console.log(`\n ** API running on port ${port} ** \n`))
